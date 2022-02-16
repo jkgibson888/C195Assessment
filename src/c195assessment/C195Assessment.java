7194074305593d6b353157b27358269c718bf272
JoshuaGibson <gibson.joshua.2020@gmail.com>
2022-02-16 02:00:50 -0500

    second save

---
+++ src/c195assessment/C195Assessment.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package c195assessment;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author techt
+ */
+public class C195Assessment extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("/View/LogInForm.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    
+}
